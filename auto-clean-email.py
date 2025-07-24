051e504b3b2cced68d4e44607a2d6e3bc7874777
ArticulateSpine <130583225+ArticulateSpine@users.noreply.github.com>
2025-07-24 13:38:55 +0530

    Delete auto-clean-email.py

--- auto-clean-email.py
+++
@@ -1,2 +0,0 @@
-import win32com.client  
-[att.SaveAsFile(f"C:/invoices/{att.FileName}") for att in win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI").Folders.Item("Inbox").Items if att.Attachments.Count]
